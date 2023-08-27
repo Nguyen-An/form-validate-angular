ec3023f480c9bdd4b7a3f48356d284eef330c1fb
An Nguyen <vanan6b@gmail.com>
2023-08-28 01:36:36 +0700

    upload code

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { ValidatorService } from './service/validator.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  @ViewChild('formRef', { static: true }) formRef: ElementRef;
+  @ViewChild('passwordRef', { static: true }) passwordRef: ElementRef;
+
+  constructor(private validatorService: ValidatorService, private elRef: ElementRef) {
+    this.formRef = elRef.nativeElement;
+    this.passwordRef = elRef.nativeElement;
+   }
+
+  ngAfterContentInit() {
+    let Validator = this.validatorService
+    let passwordRefElement = this.passwordRef.nativeElement
+    
+    Validator.initValidator({
+      form: this.formRef.nativeElement,
+      formGroupSelector: '.form-group',
+      errorSelector: '.form-message',
+      rules: [
+        Validator.isRequired('#fullname', 'Vui lòng nhập vào tên đầy đủ của bạn'),
+        Validator.isRequired('#email'),
+        Validator.isRequired('#file'),
+        Validator.isRequired('#password'),
+        Validator.isRequired('#password_confirmation'),
+        Validator.isRequired('input[name="gender"]'),
+        Validator.isRequired('#province'),
+        Validator.isEmail('#email'),
+        Validator.minLength('#password', 6),
+        Validator.isConfirmed('#password_confirmation', passwordRefElement , 'Mật khẩu nhập lại không chính xác')
+      ],
+      onSubmit: (data: any) => {
+        // Xử lý dữ liệu khi form được submit
+        console.log(data);
+      }
+    });
+  }
+
+  afterViewChecked() {
+    
+  }
+
+  ngOnInit(): void {
+    console.log();
+    
+  }
+}
